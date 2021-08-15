4a58fb288bdde5a6f92871ab128d889ba638fa88
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-15 21:10:11 +0530

    Built center of Header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import Image from "next/Image"
-import { BellIcon } from "@heroicons/react/solid"
-import { SearchIcon } from "@heroicons/react/outline"
+import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid"
+import { SearchIcon, ShoppingCartIcon, FlagIcon, PlayIcon } from "@heroicons/react/outline"
+import HeaderIcon from "./HeaderIcon"
 
 
 const Header = () => {
     return (
-        <div className="">
-            <div className="flex items-center p-2">
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+            <div className="flex items-center">
                 <img
                     loading="lazy"
                     src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
@@ -16,7 +17,16 @@ const Header = () => {
                 />
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-600" />
-                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
+                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
+                </div>
+            </div>
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon active Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
         </div>
