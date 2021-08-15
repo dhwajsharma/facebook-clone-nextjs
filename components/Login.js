8e641a887759b4c779d9a51313be7316cc82da7d
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-15 23:27:46 +0530

    Built Login page

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,17 @@
+import { signIn } from "next-auth/client"
+
 const Login = () => {
     return (
-        <div>
-
+        <div className="grid place-items-center">
+            <img
+                loading="lazy"
+                src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
+                alt="fb logo"
+                height={400}
+                width={400}
+                objectFit="contain"
+            />
+            <h1 onClick={signIn} className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
         </div>
     )
 }
